327cf31a9d6e4eb9648fcf461e28e600d604ef73
Niels Poulsen <npoulsen@uw.edu>
2019-01-15 10:13:25 -0800

    Original Commit

---
+++ src/main/java/n/poulsen/campuspaths/CampusPathsApplication.java
@@ -0,0 +1,14 @@
+package n.poulsen.campuspaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPathsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPathsApplication.class, args);
+    }
+
+}
+
